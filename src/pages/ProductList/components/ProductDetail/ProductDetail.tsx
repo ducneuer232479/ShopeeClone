7ad6d8c16e7a2b751d216fb9b214d401473609c2
ducneuer232479 <tuanduc.vu249@gmail.com>
2024-08-21 11:11:01 +0700

    feature buy now

--- src/pages/ProductList/components/ProductDetail/ProductDetail.tsx
+++ src/pages/ProductList/components/ProductDetail/ProductDetail.tsx
@@ -2,7 +2,7 @@ import { useMutation, useQuery } from '@tanstack/react-query'
 import DOMPurify from 'dompurify'
 import { useEffect, useMemo, useRef, useState } from 'react'
 
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import productApi from 'src/apis/product.api'
 import ProductRating from 'src/components/ProductRating'
 import { Product as ProductType, ProductListConfig } from 'src/types/product.type'
@@ -13,6 +13,7 @@ import purchaseApi from 'src/apis/purchase.api'
 import { queryClient } from 'src/main'
 import { purchasesStatus } from 'src/constants/purchase'
 import { toast } from 'react-toastify'
+import path from 'src/constants/path'
 
 export default function ProductDetail() {
   const [buyCount, setByCount] = useState(1)
@@ -28,6 +29,7 @@ export default function ProductDetail() {
   const [activeImage, setActiveImage] = useState('')
   const product = productDetailData?.data.data
   const imageRef = useRef<HTMLImageElement>(null)
+  const navigate = useNavigate()
   const currentImages = useMemo(
     () => (product ? product?.images.slice(...currentIndexImages) : []),
     [product, currentIndexImages]
@@ -107,6 +109,16 @@ export default function ProductDetail() {
         }
       }
     )
+  }
+
+  const buyNow = async () => {
+    const res = await addToCartMutation.mutateAsync({ buy_count: buyCount, product_id: product?._id as string })
+    const purchase = res.data.data
+    navigate(path.cart, {
+      state: {
+        purchaseId: purchase._id
+      }
+    })
   }
 
   if (!product) return null
@@ -215,7 +227,10 @@ export default function ProductDetail() {
                   />
                   Thêm vào giỏ hàng
                 </button>
-                <button className='ml-4 flex h-12 min-w-[5rem] items-center justify-center rounded-sm bg-orange px-5 capitalize text-white shadow-sm outline-none hover:bg-orange/90'>
+                <button
+                  className='ml-4 flex h-12 min-w-[5rem] items-center justify-center rounded-sm bg-orange px-5 capitalize text-white shadow-sm outline-none hover:bg-orange/90'
+                  onClick={buyNow}
+                >
                   Mua ngay
                 </button>
               </div>
